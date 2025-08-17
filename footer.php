4a15e3e9d9131570c13620c3fbf43a763a6d9cdc
zackmorgs <zackmorgenthaler@gmail.com>
2025-08-17 08:51:25 -0500

    intial commit

---
+++ footer.php
@@ -0,0 +1,9 @@
+<footer id="footer_main">
+
+  <p>&copy; <?php echo date(format: 'Y'); ?> Mobile Blast Guy</p>
+</footer>
+<?php wp_footer(); ?>
+<script src="<?php echo get_template_directory_uri(); ?>/assets/script.min.js"></script>
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+
+<head>
+  <meta charset="<?php bloginfo(show: 'charset'); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title><?php wp_title(); ?></title>
+  <?php wp_head(); ?>
+  <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri(); ?>/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri(); ?>/favicon-16x16.png">
+  <link rel="manifest" href="<?php echo get_template_directory_uri(); ?>/site.webmanifest">
+</head>
+
+<body <?php body_class(); ?>>
+  <nav id="nav_main">
+    <div class="nav-bar">
+      <a id="logo" href="/">
+        <h1>ZM</h1>
+      </a>
+
+      <button id="nav_toggle" aria-label="Toggle navigation" aria-expanded="false">
+        Menu
+      </button>
+    </div>
+    <div id="nav_links">
+      <ul class="nav-list">
+      </ul>
+    </div>
+  </nav>
